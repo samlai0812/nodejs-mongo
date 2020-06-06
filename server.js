eecf3ea1b36cd22429a7af0b346c137cb523e47e
Sam Lai <samlai0812@gmail.com>
2020-06-06 23:17:50 +0800

    good night

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+// if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+// }
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 const app = express()
 
-const indexRouter = require('./routes/index')
-
 app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('views', __dirname +'/views')
 app.set('layout','layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({limit:'10mb',extended: false}))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology : true ,
+    useNewUrlParser : true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', ()=> console.log('Connected to Mongoose'))
 
 app.listen(process.env.PORT || 3000)
