a3c7f99750ee00f051bf83d2f0e259b43d13f2f4
Sam Lai <samlai0812@gmail.com>
2020-06-08 15:18:08 +0800

    complete author views/routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 // All Authors Route
 router.get('/', async (req, res)=>{
@@ -37,6 +38,63 @@ router.post('/', async (req, res)=>{
             author: author,
             errorMessage: "Error creating Author"
         })
+    }
+})
+
+// Show author
+router.get('/:id', async (req, res)=>{
+    try{
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({author: author.id}).limit(6).exec()
+        res.render('authors/show',{
+            booksByAuthor: books,
+            author: author
+        })
+    }catch{
+        res.redirect('/')
+    }
+})
+// Edit author
+router.get('/:id/edit', async (req, res)=>{
+    try{
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit',{ author: author })
+    }catch{
+        res.redirect('authors')
+    }
+})
+// Update author
+router.put('/:id', async (req, res)=>{
+    let author
+    try{
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    }catch{
+        if ( author == null) {
+            res.redirect('/')
+        }else{
+            res.render('authors/edit',{
+                author: author,
+                errorMessage: "Error updating Author"
+            })
+        }
+    }
+})
+// Delete author
+router.delete('/:id', async (req, res)=>{
+    let author
+    try{
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors')
+    }catch{
+        if ( author == null) {
+            res.redirect('/')
+        }else{
+            res.redirect(`/authors/${ author.id }`)
+        }
     }
 })
 module.exports = router
